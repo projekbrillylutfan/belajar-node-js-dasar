3c4fc436a72f63d6607c550a7c5c8738e7c7177e
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-04-21 15:44:45 +0700

    selesai juga npm

--- node-js-npm/4_hello.js
+++ node-js-npm/4_hello.js
@@ -1,7 +1,11 @@
 import {sayHello, sum} from "billy-belajar-nodejs-npm-library";
+import {max, min} from "billy-belajar-nodejs-npm-library/number"
 
 console.log(sayHello("ayam"))
 
 const numbers = [1, 2, 3, 4, 5]
 
 console.log(sum(numbers))
+
+console.log(min(10, 20))
+console.log(max(10, 20))
