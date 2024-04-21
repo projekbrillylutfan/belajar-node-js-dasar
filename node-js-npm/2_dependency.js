ea88110ee65225f6be3d464c8bb287c652a4976f
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-04-21 11:21:04 +0700

    belajar npm walaupun saya penikmat yarn

---
+++ node-js-npm/2_dependency.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+
+const source  =  "ayam goreng rendah lemak";
+const target = _.capitalize(source);
+
+console.log(target);
---
+++ node-js-npm/index.js
@@ -0,0 +1,3 @@
+import { writeToFile } from "./1_write.js";
+
+writeToFile("hello.log", "ciat ciat")
