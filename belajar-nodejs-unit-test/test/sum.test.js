b4c95872c1c269e36b16fae2b51035d3c5ce01c6
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-04-21 18:17:53 +0700

    jest susah

---
+++ belajar-nodejs-unit-test/test/sum.test.js
@@ -0,0 +1,25 @@
+import { sum } from "../src/sum";
+
+test("test 1", () => {
+  const result = sum(1, 2);
+
+  expect(result).toBe(3);
+});
+
+test("test 2", () => {
+  const result = sum(1, 2);
+
+  expect(result).toBe(3);
+});
+
+test("test 3", () => {
+  const result = sum(1, 2);
+
+  expect(result).toBe(3);
+});
+
+test("test 4", () => {
+  const result = sum(1, 2);
+
+  expect(result).toBe(3);
+});
