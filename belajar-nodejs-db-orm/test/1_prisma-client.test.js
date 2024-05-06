7afdb83e9b747a56869af128fdad8c570829c66d
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-05-06 14:30:38 +0700

    mantap prisma

---
+++ belajar-nodejs-db-orm/test/1_prisma-client.test.js
@@ -0,0 +1,13 @@
+import {PrismaClient} from "@prisma/client";
+
+describe('prisma client', () => {
+    it("should be able to connect", async() => {
+        const prisma = new PrismaClient();
+        await prisma.$connect();
+
+
+        // do something
+
+        await prisma.$disconnect();
+    })
+})
