1b84ebfe1a13c27ca6c4f841accd003da887e586
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-05-10 20:01:41 +0700

    mantap

--- belajar-nodejs-validation/test/8_custom-validation-message.test.js
+++ belajar-nodejs-validation/test/8_custom-validation-message.test.js
@@ -36,18 +36,43 @@ describe("joi", () => {
     console.log(result);
   });
 
-  it("should can crete custom validation", () => {
-      const registerSchema = Joi.object({
-          username: Joi.string().required().min(3).max(100).email(),
-          password: Joi.string().required().min(6).max(100).custom((value, helpers) => {
-              if(value.starWith("eko")) {
-                return helpers.error("password.wrong")
-              }
-              return value;
-          }).messages({
-              "password.wrong": "password must not start with eko"
-          }),
-          confirmPassword: Joi.string().required().min(6).max(100)
+  it("should can create custom validation", function () {
+    const registerSchema = Joi.object({
+      username: Joi.string().required().min(3).max(100).email(),
+      password: Joi.string()
+        .required()
+        .min(6)
+        .max(100)
+        .custom((value, helpers) => {
+          if (value.startsWith("eko")) {
+            return helpers.error("password.wrong");
+          }
+          return value;
+        })
+        .messages({
+          "password.wrong": 'password can not start with "eko"',
+        }),
+      confirmPassword: Joi.string().required().min(6).max(100),
+    })
+      .custom((value, helpers) => {
+        if (value.password !== value.confirmPassword) {
+          return helpers.error("register.password.different");
+        }
+        return value;
       })
-  })
+      .messages({
+        "register.password.different":
+          "password and confirmPassword is different",
+      });
+
+    const request = {
+      username: "eko@pzn.com",
+      password: "12345eko",
+      confirmPassword: "salah12345",
+    };
+    const result = registerSchema.validate(request, {
+      abortEarly: false,
+    });
+    console.info(result);
+  });
 });
