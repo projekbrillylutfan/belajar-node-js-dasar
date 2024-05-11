9ad9be2e7925f8937a12f0dee01ff48620f9d40f
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-05-11 19:12:27 +0700

    register sudah selesai, tapi belum di test

--- belajar-nodejs-restfullApi-studi-kasus/src/application/web.js
+++ belajar-nodejs-restfullApi-studi-kasus/src/application/web.js
@@ -1,4 +1,10 @@
 import express from "express";
+import { errorMiddleware } from "../middleware/error-middleware.js";
+import { publicRouter } from "../route/public-api.js";
 
 export const web = express()
 web.use(express.json())
+
+web.use(publicRouter)
+
+web.use(errorMiddleware)
---
+++ belajar-nodejs-restfullApi-studi-kasus/src/service/user-service.js
@@ -0,0 +1,35 @@
+import { prismaClient } from "../application/database.js";
+import { ResponseError } from "../error/response-error.js";
+import { registerUserValidation } from "../validation/user-validation.js";
+import { validate } from "../validation/validation.js";
+import bcrypt from "bcrypt";
+
+const register = async (request) => {
+  const user = validate(registerUserValidation, request);
+
+  const countUser = await prismaClient.user.count({
+    where: {
+      username: user.username,
+    },
+  });
+
+  if (countUser === 1) {
+    throw new ResponseError(400, "username already exist");
+  }
+
+  user.password = await bcrypt.hash(user.password, 10);
+
+  const result = await prismaClient.user.create({
+    data: user,
+    select: {
+      username: true,
+      name: true,
+    },
+  });
+
+  return result;
+};
+
+export default {
+  register,
+}
